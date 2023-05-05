3e8efe0e2fe19c28e5c3f4954cf3af0380d423a9
michouclaudine <90043387+michouclaudine-code@users.noreply.github.com>
2023-05-05 16:38:08 +0200

    Added weather API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+function showTemperature(response) {
+  let temperatureElement = document.querySelector(".temp h2");
+  let temp = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = `${temp} °C/°F`;
+}
+
 function search(event) {
   event.preventDefault();
   const searchInput = document.getElementById("input-search");
   const inputValue = searchInput.value;
-  alert(inputValue);
+
+  cityElement = document.querySelector(".city h1");
+  cityElement.innerHTML = `${inputValue}`;
+
+  axios
+    .get(`${apiUrl}${inputValue}&appid=${apiKey}&units=metric`)
+    .then(showTemperature);
 }
 
 let form = document.querySelector("form");
