4861091ac868d76794b35057fbe7c16adf3ed2b3
michouclaudine <90043387+michouclaudine-code@users.noreply.github.com>
2023-05-07 15:47:05 +0200

    Updated structure

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ let apiKey = "c819171fe0abdc14039af4ef5dda283b";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 function showTemperature(response) {
-  let temperatureElement = document.querySelector(".temperature");
+  let temperatureElement = document.querySelector(".currentTemp");
   let temp = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = `${temp}`;
 
-  let humidityElement = document.querySelector("#humidity");
+  let humidityElement = document.querySelector(".humidity");
   let humidity = Math.round(response.data.main.humidity);
   humidityElement.innerHTML = `Humidity: ${humidity}%`;
 
-  let windElement = document.querySelector("#wind");
+  let windElement = document.querySelector(".wind");
   let wind = Math.round(response.data.wind.speed);
   windElement.innerHTML = `Wind: ${wind} km/h`;
 }
@@ -20,7 +20,7 @@ function search(event) {
   const searchInput = document.getElementById("input-search");
   const inputValue = searchInput.value;
 
-  cityElement = document.querySelector(".city h1");
+  let cityElement = document.querySelector(".city h1");
   cityElement.innerHTML = `${inputValue}`;
 
   axios
@@ -30,3 +30,25 @@ function search(event) {
 
 let form = document.querySelector("form");
 form.addEventListener("submit", search);
+
+// Change Celcius/Fahrenheit - currently not working will continue next week
+
+function displayCelcius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".currentTemp");
+  let celcius = parseInt(document.querySelector(".currentTemp").value);
+  temperatureElement.innerHTML = `${celcius}`;
+}
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".currentTemp");
+  let fahrenheit = (celcius * 9) / 5 + 32;
+  temperatureElement.innerHTML = `${fahrenheit}`;
+}
+
+let celciusLink = document.querySelector("#celcius");
+let fahrenheitLink = document.querySelector("#fahrenheit");
+
+celciusLink.addEventListener("click", displayCelcius);
+fahrenheitLink.addEventListener("click", displayFahrenheit);
